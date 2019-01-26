5cd2ad282235e74d1664657239912b15075c514d
AlexTrofim88 <42206009+AlexTrofim88@users.noreply.github.com>
2019-01-26 21:22:04 +0800

    Update 26-01-2019-AlexTrofim88

---
+++ Assets/Script/TerrianOnOff.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrianOnOff : MonoBehaviour {
+
+    public GameObject roomBox;
+    public GameObject terrian;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(roomBox.GetComponent<MeshRenderer>().enabled == true){
+            terrian.SetActive(true);
+        }else{
+            terrian.SetActive(false);
+        }
+	}
+}
